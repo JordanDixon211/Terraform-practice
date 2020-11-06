7dc10d867bc9fc13515d2802f5d932a0fc8082ba
Jordan Dixon <jordan.dixon211@gmail.com>
2020-11-06 00:47:44 +0000

    add security group

--- prod.tf
+++ prod.tf
@@ -11,3 +11,34 @@ resource "aws_s3_bucket" "prod_tf_course" {
     bucket = "tf-course-2020-05-11"
     acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+    name        = "prod_web"
+    description = "Allow standard http and https ports inbound and everything outbound"
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["31.205.106.93/32"] 
+    }
+       ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["31.205.106.93/32"] 
+    }
+
+    egress {
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+        cidr_blocks = ["31.205.106.93/32"] 
+    }
+
+    tags = {
+        "terraform" : "true"
+    }
+}
