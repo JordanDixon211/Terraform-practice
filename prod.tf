e176591f9ce990659d06ea5f8ba762101434d411
Jordan Dixon <jordan.dixon211@gmail.com>
2020-11-10 22:32:36 +0000

    add prod.tf

--- prod.tf
+++ prod.tf
@@ -21,23 +21,40 @@ resource "aws_security_group" "prod_web" {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = ["31.205.106.93/32"] 
+        cidr_blocks = ["0.0.0.0/0"] 
     }
        ingress {
         from_port   = 443
         to_port     = 443
         protocol    = "tcp"
-        cidr_blocks = ["31.205.106.93/32"] 
+        cidr_blocks = ["0.0.0.0/0"] 
     }
 
     egress {
         from_port = 0
         to_port   = 0
         protocol  = "-1"
-        cidr_blocks = ["31.205.106.93/32"] 
+        cidr_blocks = ["0.0.0.0/0"] 
     }
 
     tags = {
         "terraform" : "true"
     }
+}
+
+resource "aws_instance" "prod_web" {
+  ami                    = "ami-00c63e37cd499a3dd"
+  instance_type          = "t2.nano"
+  vpc_security_group_ids = ["${aws_security_group.prod_web.id}"]
+  tags                    = {
+        "terraform" : "true"
+    }
+}
+
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
+
+  tags = {
+      "Terraform" : "true"
+  }
 }
